fce081f4e739b3824e02864b1cb81c23ac7cbb8b
puri calvo <puri_adrian@hotmail.com>
2026-02-12 20:41:21 +0100

    Deploy Lambda fin Backend

--- main.go
+++ main.go
@@ -127,8 +127,10 @@ func ValidoParametros() bool {
 // ===== Función de headers CORS =====
 func corsHeaders() map[string]string {
 	return map[string]string{
-		"Access-Control-Allow-Origin":  "*",
-		"Access-Control-Allow-Headers": "*",
-		"Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+		"Content-Type":                     "application/json",
+		"Access-Control-Allow-Origin":      "http://localhost:3000",
+		"Access-Control-Allow-Credentials": "true",
+		"Access-Control-Allow-Headers":     "Content-Type,Authorization",
+		"Access-Control-Allow-Methods":     "GET,POST,PUT,DELETE,OPTIONS",
 	}
 }
