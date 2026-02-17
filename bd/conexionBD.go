cc0c16ab33a51cf7ada62967de7e0c86cd2baa1e
puri calvo <puri_adrian@hotmail.com>
2026-02-17 22:46:48 +0100

    Deploy Lambda fin Backend

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -3,7 +3,6 @@ package bd
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/puricalvo/twitterGo/models"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,6 +10,39 @@ import (
 )
 
 var MongoCN *mongo.Client
+var DatabaseName string
+
+func ConectarBD(ctx context.Context) error {
+	user := ctx.Value(models.Key("user")).(string)
+	passwd := ctx.Value(models.Key("password")).(string)
+	host := ctx.Value(models.Key("host")).(string)
+	connStr := fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority", user, passwd, host)
+
+	var clientOptions = options.Client().ApplyURI(connStr)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Conexión Exitosa con la BD")
+	MongoCN = client
+	DatabaseName = ctx.Value(models.Key("database")).(string)
+	return nil
+}
+
+func BaseContectada() bool {
+	err := MongoCN.Ping(context.TODO(), nil)
+	return err == nil
+}
+
+/* var MongoCN *mongo.Client
 var DatabaseName string
 
 func ConectarBD(ctx context.Context) error {
@@ -58,3 +90,4 @@ func BaseContectada() bool {
 	return err == nil
 }
 
+ */
