3dc1bd806a23c9dee65c795a75303fbfa81888d1
puri calvo <puri_adrian@hotmail.com>
2026-02-24 19:42:25 +0100

    Deploy Lambda fin Backend

--- routers/obtengoImagen.go
+++ routers/obtengoImagen.go
@@ -17,6 +17,167 @@ import (
 )
 
 func ObtenerImagen(ctx context.Context, uploadType string, request events.APIGatewayProxyRequest, claim models.Claim) models.RespApi {
+
+	var r models.RespApi
+	r.Status = 400
+
+	ID := request.QueryStringParameters["id"]
+	if len(ID) < 1 {
+		r.Message = "El parámetro ID es obligatorio"
+		return r
+	}
+
+	perfil, err := bd.BuscoPerfil(ID)
+	if err != nil {
+		r.Message = "Usuario no encontrado " + err.Error()
+		return r
+	}
+
+	
+
+	var filename string
+	switch uploadType {
+	case "A":
+		filename = perfil.Avatar
+	case "B":
+		filename = perfil.Banner
+    case "T":
+        // 1. Obtenemos el nombre del parámetro
+        nombre := request.QueryStringParameters["nombre"]
+        if len(nombre) < 1 {
+            r.Message = "El parámetro nombre es obligatorio"
+            return r
+        }
+
+        // 2. IMPORTANTE: 
+        // Si el nombre que viene de la base de datos ya incluye "tweetImage/", 
+        // filename será igual a nombre.
+        // Si NO lo incluye, hay que añadírselo.
+        if !strings.HasPrefix(nombre, "tweetImage/") {
+            filename = "tweetImage/" + nombre
+        } else {
+            filename = nombre
+        }
+    
+	}
+
+	fmt.Println("Filename " + filename)
+	svc := s3.NewFromConfig(awsgo.Cfg)
+
+	file, err := downloadFromS3(ctx, svc, filename)
+	if err != nil {
+		r.Status = 500
+		r.Message = "Error descargando archivo de S3 " + err.Error()
+		return r
+	}
+
+    
+
+	encoded := base64.StdEncoding.EncodeToString(file.Bytes())
+
+	r.CustomResp = &events.APIGatewayProxyResponse{
+    StatusCode:      200,
+    Body:            encoded,
+    IsBase64Encoded: true,
+    Headers: map[string]string{
+        "Content-Type": "image/jpeg",
+        "Access-Control-Allow-Origin": "*",
+    },
+}
+	return r
+}
+
+func downloadFromS3(ctx context.Context, svc *s3.Client, filename string) (*bytes.Buffer, error) {
+	bucket := ctx.Value(models.Key("bucketName")).(string)
+	obj, err := svc.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer obj.Body.Close()
+	fmt.Println("bucketname = " + bucket)
+
+	file, err := io.ReadAll(obj.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := bytes.NewBuffer(file)
+
+	return buffer, nil
+}
+
+/* func ObtenerImagen(ctx context.Context, uploadType string, request events.APIGatewayProxyRequest, claim models.Claim) models.RespApi {
+
+	var r models.RespApi
+	r.Status = 400
+
+	ID := request.QueryStringParameters["id"]
+	if len(ID) < 1 {
+		r.Message = "El parámetro ID es obligatorio"
+		return r
+	}
+
+	perfil, err := bd.BuscoPerfil(ID)
+	if err != nil {
+		r.Message = "Usuario no encontrado " + err.Error()
+		return r
+	}
+
+	var filename string
+	switch uploadType {
+	case "A":
+		filename = perfil.Avatar
+	case "B":
+		filename = perfil.Banner
+	}
+
+	fmt.Println("Filename " + filename)
+	svc := s3.NewFromConfig(awsgo.Cfg)
+
+	file, err := downloadFromS3(ctx, svc, filename)
+	if err != nil {
+		r.Status = 500
+		r.Message = "Error descargando archivo de S3 " + err.Error()
+		return r
+	}
+
+	r.CustomResp = &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       file.String(),
+		Headers: map[string]string{
+			"Content-Type":        "application/octet-stream",
+			"Content-Disposition": fmt.Sprintf("attachment; filename=\"%s\"", filename),
+		},
+	}
+	return r
+}
+
+func downloadFromS3(ctx context.Context, svc *s3.Client, filename string) (*bytes.Buffer, error) {
+	bucket := ctx.Value(models.Key("bucketName")).(string)
+	obj, err := svc.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer obj.Body.Close()
+	fmt.Println("bucketname = " + bucket)
+
+	file, err := ioutil.ReadAll(obj.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := bytes.NewBuffer(file)
+
+	return buffer, nil
+}
+ */
+/* func ObtenerImagen(ctx context.Context, uploadType string, request events.APIGatewayProxyRequest, claim models.Claim) models.RespApi {
     var r models.RespApi
     r.Status = 400
 
@@ -110,4 +271,4 @@ func downloadFromS3(ctx context.Context, svc *s3.Client, filename string) (*byte
 	buffer := bytes.NewBuffer(file)
 
 	return buffer, nil
-}
+} */
